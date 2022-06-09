6f20cc9f56d937c61902eb48de11be94465df46a
anqi-guo <guo.anqi@icloud.com>
2022-06-09 23:44:14 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Table of Contents
+1. Installation
+2. Project Motivation
+3. Fle Descriptions
+4. Results
+5. Licensing, Authors, and Acknowledgements
+
+# Installation
+Libraries required for this project can be found in `requirements.txt`
+
+# Project Motivation
+This is the second project of Udacity Data Scientist Nano Degree program, aiming to practice our skills on using Scikit-learn's `Pipeline`. 
+Pipeline is a productivity tool that collapses all preprocessing and modeling steps into a one function call. Besides, we practice other
+skills like ETL, and web development, which are also important for data scientist.
+
+# File Descriptions
+- app
+  - templates
+    - master.html: this is the main page of the application
+    - go.html: this displays the classification result
+  - run.py: this is the python file that runs the application
+  - mainpage.png: this is the snapshot of main page of the application
+  - resultpage.png: this is the snapshot of result page of the application
+- data
+  - categories.csv: this contains all the categories of the messages
+  - messages.csv: this contains all the messages content
+  - DisasterResponse.db: this is the sqlite db file that contains cleaned data
+  - process_data.py: this is the python file that does the etl
+  - ETL Pipeline Preparation.ipynb
+- models
+  - train_classifier.py: this is the python file that trains the classifier
+  - model.pkl: this is the final model
+  - ML Ppeline Preparation.ipynb
+
+# Results
+Below is a snapshot of the main page
+![mainpage](https://github.com/anqi-guo/udacity-dsnd-project2/blob/main/mainpage.png)
+Below is a snapshot of the classification result page
+![resultpage](https://github.com/anqi-guo/udacity-dsnd-project2/blob/main/resultpage.png)
+
+# Licensing, Authors, and Acknowledgements
+The data was provided by [Appen](https://appen.com/)
---
+++ models/train_classifier.py
@@ -0,0 +1,108 @@
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+import regex as re
+import nltk
+nltk.download(['punkt', 'stopwords', 'wordnet', 'omw-1.4'])
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+import pickle
+
+
+def load_data(database_filepath):
+    engine = create_engine(database_filepath)
+    df = pd.read_sql_table('disaster_response', con=engine)
+    X = df['message']
+    Y = df.iloc[:, 4:]
+    return X, Y
+
+
+def tokenize(text):
+    # replace url
+    text = re.sub('http[^ ]+', 'urlplaceholder', text)
+
+    # uncapitalize
+    text = text.lower()
+
+    # remove punctuation
+    text = re.sub(r'\p{P}+', ' ', text)
+
+    # remove extra space
+    text = re.sub('\s+', ' ', text)
+    text = text.strip()
+
+    # tokenize
+    token_list = word_tokenize(text)
+
+    # remove stopwords
+    sw = stopwords.words('english')
+    token_list = [w for w in token_list if w not in sw]
+
+    # lemmatize
+    lemmatizer = WordNetLemmatizer()
+    token_list = [lemmatizer.lemmatize(w) for w in token_list]
+
+    return token_list
+
+
+def build_model():
+    pipeline = Pipeline([
+        ('tfidf', TfidfVectorizer(tokenizer=tokenize)),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+
+    return pipeline
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    Y_pred = model.predict(X_test)
+    for i in range(Y_test.shape[1]):
+        y_true = Y_test.iloc[:, i]
+        y_pred = Y_pred[:, i]
+        print('-' * 50)
+        print(Y_test.columns[i])
+        print(classification_report(y_true, y_pred))
+
+
+def save_model(model, model_filepath):
+    pickle.dump(model.best_estimator_, open(model_filepath, 'wb'))
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+        print('Building model...')
+        model = build_model()
+
+        print('Training model...')
+        model.fit(X_train, Y_train)
+
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database ' \
+              'as the first argument and the filepath of the pickle file to ' \
+              'save the model to as the second argument. \n\nExample: python ' \
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
